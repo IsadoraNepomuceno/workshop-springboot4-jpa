d16e66876304a5fe23bed127dac676e1cf4ea944
Isadora Nepomuceno <iisadoranepomuceno@gmail.com>
2023-03-28 10:38:39 -0300

    Category

---
+++ src/main/java/com/cursopoo/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.cursopoo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursopoo.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+}
