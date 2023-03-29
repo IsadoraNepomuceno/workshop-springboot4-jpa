b961726903b11a4a536138534917e939f5a5280a
Isadora Nepomuceno <iisadoranepomuceno@gmail.com>
2023-03-29 11:29:56 -0300

    Payment, one-to-one association

--- src/main/java/com/cursopoo/course/config/TestConfig.java
+++ src/main/java/com/cursopoo/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.cursopoo.course.entities.Category;
 import com.cursopoo.course.entities.Order;
 import com.cursopoo.course.entities.OrderItem;
+import com.cursopoo.course.entities.Payment;
 import com.cursopoo.course.entities.Product;
 import com.cursopoo.course.entities.User;
 import com.cursopoo.course.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
